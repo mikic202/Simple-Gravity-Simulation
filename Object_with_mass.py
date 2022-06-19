2ebe96eb05a061d57b756d072baa3dd42ce9b892
Mikołaj Chomanski <mikolaj.chomanski@gmail.com>
2022-06-19 20:52:21 +0200

    added mass object constructor

--- Object_with_mass.py
+++ Object_with_mass.py
@@ -2,5 +2,8 @@
 
 
 class MassObject:
-    def __init__(self) -> None:
-        pass
+    def __init__(self, mass:int, position:tuple, speed:int = 0, acceleration:int = 0) -> None:
+        self._mass = mass
+        self._speed = position
+        self._acceleration = acceleration
+        self._position = position
