00dfd809fe0fba0365941e188fc24f7e05e19743
Mikołaj Chomanski <mikolaj.chomanski@gmail.com>
2022-06-25 10:30:26 +0200

    fixed Gravity phisics

--- GravityPhisics.py
+++ GravityPhisics.py
@@ -1,5 +1,34 @@
+from math import sqrt
+from Object_with_mass import MassObject
+from Vector import Vector
+
+
+G = 6.67 * 10**-11
 
 
 class GravitiPhisics:
     def __init__(self) -> None:
         pass
+
+    def update_objects(self, objects:set):
+        for object in objects:
+            object.uptade()
+        for object in objects:
+            object:MassObject
+            f = 0
+            for object2 in objects:
+                if(object2 != object):
+                    object2:MassObject
+                    r_x = (object2.position().x() - object.position().x())*10**7
+                    r_y = (object2.position().y() - object.position().y())*10**7
+                    if(abs(r_x)<10):
+                        f = 1
+                    a_g = G * object2.mass()*10**24/(r_x**2 + r_y**2)
+                    a_x = sqrt(a_g**2 / (abs(r_y/r_x)**2 + 1)) * (r_x/abs(r_x))
+                    a_y = sqrt(a_g**2 / (abs(r_x/r_y)**2 + 1)) * (r_y/abs(r_y))
+                    # if(r_y/r_x > 10 * r_y):
+                    #     a_y = a_g * (r_y/abs(r_y))
+                    # if(r_x/r_y > 10 * r_x):
+                    #     a_x = a_g * (r_x/abs(r_x))
+                    object.set_acceleration(Vector(a_x, a_y))
+
--- Window.py
+++ Window.py
@@ -8,13 +8,15 @@ from Vector import Vector
 start_width = 800
 start_height = 800
 first_object_pos = Vector(0, 0)
-second_object_pos = Vector(500, 500)
+second_object_pos = Vector(800, 800)
+first_object_v = Vector(0.05, 0)
+second_object_v = Vector(-0.05, 0)
 
 
 class Window:
     def __init__(self) -> None:
         self._WIN = pygame.display.set_mode((start_width, start_height))
-        self._objects = {MassObject("a", 10, first_object_pos), MassObject("b", 10, second_object_pos)}
+        self._objects = {MassObject("a", 10, first_object_pos, first_object_v), MassObject("b", 10, second_object_pos, second_object_v)}
         self._phisics = GravitiPhisics()
         self._start()
 
@@ -26,6 +28,8 @@ class Window:
                     pygame.quit()
                     sys.exit()
             for object in self._objects:
+                print(object.position().y())
+                print("\n")
                 pygame.draw.circle(self._WIN, (255, 255, 255), [object.position().x(), object.position().y()], 20)
             pygame.display.update()
             self._WIN.fill((0, 0, 0))
