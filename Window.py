c262de8bff56f4c25a0babdf3914526509ba2dff
Mikołaj Chomanski <mikolaj.chomanski@gmail.com>
2022-06-26 12:54:34 +0200

    created simple example

--- Window.py
+++ Window.py
@@ -7,18 +7,25 @@ from Vector import Vector
 
 start_width = 800
 start_height = 800
-first_object_pos = Vector(0, 0)
-second_object_pos = Vector(400, 693)
-first_object_v = Vector(0.05, 0)
-second_object_v = Vector(-0.05, 0)
-third_object_pos = Vector(800, 1)
-third_object_v = Vector(0.0, 0.05)
+# first_object_pos = Vector(0, 0)
+# second_object_pos = Vector(400, 693)
+# first_object_v = Vector(0.05, -0.029)
+# second_object_v = Vector(-0.05, -0.029)
+# third_object_pos = Vector(800, 1)
+# third_object_v = Vector(0.029, 0.05)
+
+first_object_pos = Vector(200, 100)
+second_object_pos = Vector(800, 800)
+first_object_v = Vector(1.4, 0)
+second_object_v = Vector(-1, 0)
+third_object_pos = Vector(400, 400)
+third_object_v = Vector(0, 0)
 
 
 class Window:
     def __init__(self) -> None:
         self._WIN = pygame.display.set_mode((start_width, start_height))
-        self._objects = {MassObject("a", 10, first_object_pos, first_object_v), MassObject("b", 10, second_object_pos, second_object_v), MassObject("c", 10, third_object_pos, third_object_v)}
+        self._objects = {MassObject("a", 1.5, first_object_pos, first_object_v), MassObject("b", .00001, second_object_pos, second_object_v), MassObject("c", 1000, third_object_pos, third_object_v)}
         self._phisics = GravitiPhisics()
         self._start()
 
