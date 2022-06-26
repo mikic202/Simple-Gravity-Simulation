0f78ff55197fcf94acbb72d9c0b3402c9a12db4e
Mikołaj Chomanski <mikolaj.chomanski@gmail.com>
2022-06-26 13:25:28 +0200

    added init tests

--- test_mass_object.py
+++ test_mass_object.py
@@ -1,5 +1,22 @@
 from Object_with_mass import MassObject
+from Vector import Vector
 
 
-def test_test():
-    assert True
+def test_init_with_all_param():
+    object = MassObject("nam", 10, Vector(1, 2, 3), Vector(4, 5), Vector(8, 7))
+
+    assert object.name() == "nam"
+    assert object.mass() == 10
+    assert object.position().x() == 1
+    assert object.speed().x() == 4
+    assert object.acceleration().x() == 8
+
+
+def test_init_only_needed_param():
+    object = MassObject("nam", 10, Vector(1, 2, 3))
+
+    assert object.name() == "nam"
+    assert object.mass() == 10
+    assert object.position().x() == 1
+    assert object.speed().x() == 0
+    assert object.acceleration().x() == 0
