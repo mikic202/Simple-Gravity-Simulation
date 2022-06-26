4957888066db6399e948dc0737dd8e207a391733
Mikołaj Chomanski <mikolaj.chomanski@gmail.com>
2022-06-26 13:34:15 +0200

    added setter tests

--- test_mass_object.py
+++ test_mass_object.py
@@ -20,3 +20,38 @@ def test_init_only_needed_param():
     assert object.position().x() == 1
     assert object.speed().x() == 0
     assert object.acceleration().x() == 0
+
+
+def test_set_name():
+    object = MassObject("nam", 10, Vector(1, 2, 3))
+    object.set_name("123")
+
+    assert object.name() == "123"
+
+
+def test_set_mass():
+    object = MassObject("nam", 10, Vector(1, 2, 3))
+    object.set_mass(12)
+
+    assert object.mass() == 12
+
+
+def test_set_position():
+    object = MassObject("nam", 10, Vector(1, 2, 3))
+    object.set_position(Vector(4, 5))
+
+    assert object.position().x() == 4
+
+
+def test_set_speed():
+    object = MassObject("nam", 10, Vector(1, 2, 3))
+    object.set_speed(Vector(2, 2, 2))
+
+    assert object.speed().x() == 2
+
+
+def test_set_acceleration():
+    object = MassObject("nam", 10, Vector(1, 2, 3))
+    object.set_acceleration(Vector(4, 4))
+
+    assert object.acceleration().x() == 4
